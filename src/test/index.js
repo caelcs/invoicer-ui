ac780d16018dbabb50f44bb2c6ae2a561c9ffc6d
Adolfo Secchi <adolfo.secchi.external@ynap.com>
2017-10-04 15:59:48 +0100

    fixing unit test

--- src/test/index.js
+++ src/test/index.js
@@ -1,13 +1,6 @@
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-import chaiJsx from 'chai-jsx';
 import {jsdom} from 'jsdom';
-
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-chai.use(chaiEnzyme());
-chai.use(chaiJsx);
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
