37ac5e825e3e564f32f511e68ff9b5532e513862
Adolfo Secchi <adolfo.secchi.external@ynap.com>
2017-10-04 15:06:13 +0100

    adding chai dependencies

--- src/test/index.js
+++ src/test/index.js
@@ -3,12 +3,17 @@ import chaiEnzyme from 'chai-enzyme';
 import chaiJsx from 'chai-jsx';
 import {jsdom} from 'jsdom';
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 chai.use(chaiEnzyme());
 chai.use(chaiJsx);
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
+
+configure({ adapter: new Adapter() });
 
 function noop () {
   return null;
